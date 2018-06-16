209485efb0f68487de4835480fd11ff3a4821d8e
shawnc8160 <shawnc8160@gmail.com>
2018-06-16 16:54:20 -0700

    Have win condition logic figured out, implemented for horizontal only

--- project1/js/app.js
+++ project1/js/app.js
@@ -7,43 +7,123 @@ const App ={
   // Properties for the game
   // -----------------
   curPlayer: 'red',
+  winAmount: 3,
   gameCount: 0,
+  checkRowNum: 0,
+  checkColNum: 0,
+  checkCount: 0,
 
   // -----------------
-  // Ends the current turn
+  // Starts the current turn
   // -----------------
   startTurn: function() {
     // Swaps player
     this.curPlayer = (this.curPlayer === 'red') ? 'blue' : 'red';
     // Display message for whose turn
     $('#msg').text(`${this.curPlayer} turn`);
-    // Create new chip (div) and append to the correct side bar
+    // Create new chip (div)
     $newChip = $('<div>').addClass(this.curPlayer).draggable({ snap: ".ui-droppable" });
+    // Append new chip to the correct side bar
     $(`#${this.curPlayer}_container`).append($newChip);
   },
 
   // -----------------
   // Ends the current turn
   // -----------------
-  endTurn: function() {
+  endTurn: function($cell) {
     // Keep count of how many turns (maybe for future use)
     this.gameCount++;
 
-    // Might put logic here to check for end game
+    // Check if there's a winner
+    this.winCheck($cell);
 
     // Call next function to start next turn
     this.startTurn();
-  }
+  },
 
-  // // -----------------
-  // // Checks the column for available cell
-  // // -----------------
-  // checkOpenCell: function() {
-  //   // Get the column ID
-  //   // Retrieve all cells in the column
-  //   // Check which cell is available by looking for class 'none'
-  //   // Choose the earliest one
-  //   // Return the object if found
-  //   // Return null if not
-  // }
-}
+  // -----------------
+  // Evaluates if there's a winner
+  // -----------------
+  winEval: function() {
+    console.log(`Check count is: ${this.checkCount}`);
+    // Check if we have a winner?
+    if (this.checkCount >= this.winAmount) {
+        //We have a winner
+        console.log(`${this.curPlayer} has won the game`);
+        // Reset the game and take score
+
+    } else {
+      this.checkCount = 0;
+    }
+  },
+
+  // -----------------
+  // Check win, calls other helper functions
+  // -----------------
+  winCheck: function($cell) {
+    console.log("Check win has ", $cell);
+    // Check horizontally for a winner
+    this.checkCell($cell, 0, 1);
+    this.checkCell($cell, 0, -1);
+    // Check for winner
+    this.winEval();
+
+
+    // After all the checks, reset the checkRowNum and checkColNum
+    this.checkRowNum = 0;
+    this.checkColNum = 0;
+  },
+
+  // -----------------
+  // Checks horizontal,
+  // Parameters: $cell - the current cell (as jquery object)
+  //              row - if negative: looks down
+  //                    if positive, looks up
+  //                    if 0, stays on the same row
+  //              col - if negative: looks left
+  //                    if positive, looks right
+  //                    if 0, stays on the same column
+  // -----------------
+  checkCell: function($cell, row, col) {
+
+    // What's the current cell's position
+    this.checkRowNum = Number($cell.attr('rownum'));
+    this.checkColNum = Number($cell.attr('colnum'));
+    console.log(`Checking horizontal for row: ${this.checkRowNum}, col: ${this.checkColNum}`);
+
+    // Check for the next cell (this is dependent on what was passed)
+    let rowNum = this.checkRowNum+row;
+    let colNum = this.checkColNum+col;
+    let $nextCell = $(`.col[rownum=${rowNum}][colnum=${colNum}]`);
+    console.log('next cell is:', $nextCell);
+    let matchFound = true;
+    //  Loop and check the cells in certain direction (direction dependent on parameters)
+    //  Keep checking as long as these conditions are true:
+    //  1. Its valid position on the board, so its not off the board
+    //  2. The cell actually matches current player's color
+    //  3. We found 4 matches and the player won
+    // console.log(`rowNum: ${rowNum} colNum: ${colNum} UI.rowNum: ${UI.rowNum} UI.colNum: ${UI.colNum}`);
+    while(
+      (rowNum > 0) && (colNum > 0) &&
+      (rowNum <= UI.rowNum) && (colNum <= UI.colNum) &&
+      (matchFound) && (this.checkCount <= this.winAmount)) {
+      // Check if the cell matches
+      // console.log($nextCell.hasClass(`${this.curPlayer}`));
+      if ($nextCell.hasClass(`${this.curPlayer}`)) {
+        console.log('Found match');
+        // If it does, increment checkCount
+        this.checkCount++;
+
+        // Get the next cell
+        rowNum += row;
+        colNum += col;
+        $nextCell = $(`.col[rownum=${rowNum}][colnum=${colNum}]`);
+      } else {
+        // No match, stop the loop
+        matchFound = false;
+      }
+    } // End while loop here
+
+  }// Close checkCell function
+
+} // Close App object
--- project1/js/eventhandlers.js
+++ project1/js/eventhandlers.js
@@ -8,10 +8,11 @@ const EventHandler = {
   // UI Properties
   // -----------------
   inputHandler: function (event, ui) {
-    console.log("Handler called");
-    // Get the column ID
-    const colNum = $(event.currentTarget).attr('inputcolnum');
-    console.log($(event.currentTarget).attr('inputcolnum'));
+    // Get the input column
+    const $inputCol = $(event.currentTarget);
+    $inputCol.css('opacity', '.5');
+    // Get the column number
+    const colNum = $inputCol.attr('inputcolnum');
     // Retrieve all cells in the column
     // Check which cell is available by looking for class 'none'
     const $cols = $(`div[colnum=${colNum}]`).filter('.none');
@@ -19,13 +20,13 @@ const EventHandler = {
     if ($cols.length > 0) {
       // Choose the earliest one
       const $cell = $cols.eq($cols.length-1)
-      console.log(`cell selected is: ${$cell}`);
       // Set cell for current player
       $cell.css('background', App.curPlayer);
       $cell.removeClass('none').addClass(App.curPlayer);
+      // Remove draggable item (might just want to remove draggable property and do animation)
       ui.draggable.remove();
       // End the turn
-      App.endTurn();
+      App.endTurn($cell);
     }
   }
 }
