40c3912403d5436d3521adfa2af9bef3908674d3
shawnc8160 <shawnc8160@gmail.com>
2018-08-25 20:32:49 -0700

    Made changes for responsive

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(() => {
+
+  console.log('Starting');
+  // Click handler to expand and contract resume section
+  $('#resume-header').click((event) => {
+    if ($('.resume-container').is( ":hidden" )) {
+      $('.resume-container').slideDown( "slow" );
+      $('.resume-container').css('display', 'flex');
+    } else {
+      $('.resume-container').slideUp( "slow" );
+    }
+    event.stopImmediatePropagation();
+  })
+
+  // Click handler to expand and contract resume section
+  $('#portfolio-header').click((event) => {
+    if ($('.portfolio-container').is( ":hidden" )) {
+      $('.portfolio-container').slideDown( "slow" );
+    } else {
+      $('.portfolio-container').slideUp( "slow" );
+    }
+    event.stopImmediatePropagation();
+  })
+
+})
+
+// $('.resume-container').css('display', 'none');
